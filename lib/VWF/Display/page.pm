1872a33e18b3f9e83835930058e3b28ff2b7066c
Nigel Horne <njh@bandsman.co.uk>
2016-11-16 18:01:20 -0500

    Added EnvHash for Travis

--- lib/VWF/Display/page.pm
+++ lib/VWF/Display/page.pm
@@ -7,7 +7,7 @@ use Config::Auto;
 use CGI::Info;
 use File::Spec;
 use Template::Filters;
-use Template::Plugin::Envhash;
+use Template::Plugin::EnvHash;
 use HTML::SocialMedia;
 
 my %blacklist = (
