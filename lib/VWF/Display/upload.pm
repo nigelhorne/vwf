578f3c5b7345a1e98af39a497bc0381117932aba
Nigel Horne <njh@bandsman.co.uk>
2018-01-01 20:57:15 -0500

    Started code to save the sample blog page

--- lib/VWF/Display/upload.pm
+++ lib/VWF/Display/upload.pm
@@ -1,7 +1,41 @@
 package VWF::Display::upload;
 
+use warnings;
+use strict;
+
 use VWF::Display;
 
 our @ISA = ('VWF::Display');
+
+sub html {
+	my $self = shift;
+	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+
+	my $info = $self->{_info};
+	my $allowed = {
+		'page' => 'upload',
+		'action' => 'publish',	# TODO: regex of allowable name formats
+		'title' => undef,
+		'contents' => undef,
+		'lang' => qr/^[A-Z][A-Z]/i,
+	};
+	my %params = %{$info->params({ allow => $allowed })};
+
+	delete $params{'page'};
+	delete $params{'lang'};
+
+	unless(scalar(keys %params)) {
+		# Display a blank editor page
+		return $self->SUPER::html();
+	}
+
+	open(my $fout, '>>', '/tmp/NJH');
+
+	use Data::Dumper;
+
+	print $fout Data::Dumper->new([\%params])->Dump();
+
+	return $self->SUPER::html({ published => 1 });
+}
 
 1;
