165296f27189e6863a24315e94f60909b29aab3d
Nigel Horne <njh@bandsman.co.uk>
2023-08-03 13:54:20 -0400

    Combine all args and remove Google Plus

--- lib/VWF/Display.pm
+++ lib/VWF/Display.pm
@@ -159,9 +159,10 @@ sub new {
 	my $self = {
 		_config => $config,
 		_info => $info,
-		_lingua => $args{lingua},
-		_logger => $args{logger},
-		_cachedir => $args{cachedir},
+		# _lingua => $args{lingua},
+		# _logger => $args{logger},
+		# _cachedir => $args{cachedir},
+		%args,
 		# _page => $info->param('page'),
 	};
 
@@ -178,7 +179,7 @@ sub new {
 		$sm = HTML::SocialMedia->new({ cache => $smcache, lingua => $args{lingua}, logger => $args{logger} });
 	}
 	$self->{'_social_media'}->{'facebook_share_button'} = $sm->as_string(facebook_share_button => 1);
-	$self->{'_social_media'}->{'google_plusone'} = $sm->as_string(google_plusone => 1);
+	# $self->{'_social_media'}->{'google_plusone'} = $sm->as_string(google_plusone => 1);
 
 	return bless $self, $class;
 }
