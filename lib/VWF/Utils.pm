bb4f729b504e7b65c067a3538312d405ee744cac
Nigel Horne <njh@bandsman.co.uk>
2023-08-12 18:38:20 -0700

    Getting ready for the latest CHI::Driver::SharedMem

--- lib/VWF/Utils.pm
+++ lib/VWF/Utils.pm
@@ -108,7 +108,7 @@ sub create_memory_cache {
 		}
 		# return CHI->new(driver => 'Memcached', servers => [ '127.0.0.1:11211' ], namespace => $args{'namespace'});
 		# return CHI->new(driver => 'File', root_dir => '/tmp/cache', namespace => $args{'namespace'});
-		return CHI->new(driver => 'SharedMem', size => 16 * 1024, max_size => 16 * 1024, shmkey => 98766789, namespace => $args{'namespace'});
+		return CHI->new(driver => 'SharedMem', max_size => 1024, shm_size => 16 * 1024, shm_key => 98766789, namespace => $args{'namespace'});
 }
 	if($logger) {
 		$logger->debug('memory cache via ', $config->{memory_cache}->{driver}, ', namespace: ', $args{'namespace'});
@@ -144,9 +144,12 @@ sub create_memory_cache {
 		}
 		$chi_args{'servers'} = \@servers;
 	} elsif($driver eq 'SharedMem') {
-		$chi_args{'shmkey'} = $args{'shmkey'} || $config->{memory_cache}->{shmkey};
-		if(my $size = ($args{'size'} || $config->{'memory_cache'}->{'size'})) {
-			$chi_args{'max_size'} = $chi_args{'size'} = $size;
+		$chi_args{'shm_key'} = $args{'shm_key'} || $config->{memory_cache}->{shm_key};
+		if(my $shm_size = ($args{'shm_size'} || $config->{'memory_cache'}->{'shm_size'})) {
+			$chi_args{'shm_size'} = $size;
+		}
+		if(my $max_size = ($args{'max_size'} || $config->{'memory_cache'}->{'max_size'})) {
+			$chi_args{'max_size'} = $size;
 		}
 	} elsif(($driver ne 'Null') && ($driver ne 'Memory') && ($driver ne 'SharedMem')) {
 		$chi_args{'root_dir'} = $args{'root_dir'} || $config->{memory_cache}->{root_dir};
