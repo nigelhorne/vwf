5af6f98d114f6dc7cf408553e77bd660ffda009c
Nigel Horne <njh@bandsman.co.uk>
2025-02-05 12:32:15 -0800

    Send the browser information to the meta_data page to be displayed

--- lib/VWF/Display/meta_data.pm
+++ lib/VWF/Display/meta_data.pm
@@ -9,4 +9,23 @@ use VWF::Display;
 
 our @ISA = ('VWF::Display');
 
+sub html
+{
+	my $self = shift;
+        my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+
+	my $vwf_log = $args{'vwf_log'};
+
+	my $datapoints;
+	foreach my $type('web', 'mobile', 'search', 'robot') {
+		my @entries = $vwf_log->type({ type => $type });
+		$datapoints .= '{y: ' . scalar(@entries) . ", label: \"$type\"},\n";
+		if($self->{'logger'}) {
+			$self->{'logger'}->debug("$type = " . scalar(@entries));
+		}
+	}
+
+	return $self->SUPER::html({ datapoints => $datapoints });
+}
+
 1;
