571aaec44f95b2d8b79a3e3d691032d703c2b1c7
Nigel Horne <njh@bandsman.co.uk>
2025-09-02 09:51:29 -0700

    Revert meta_data page

--- lib/VWF/Display/meta_data.pm
+++ lib/VWF/Display/meta_data.pm
@@ -6,26 +6,25 @@ use warnings;
 # Display the meta-data page - the internal status of the server and VWF system
 
 use parent 'VWF::Display';
-use JSON::MaybeXS;
 
-sub html {
+sub html
+{
 	my $self = shift;
-	my %args = ref($_[0]) eq 'HASH' ? %{$_[0]} : @_;
+	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
 	my $vwf_log = $args{'vwf_log'} or die "Missing 'vwf_log' handle";
 	my $domain_name = $self->{'info'}->domain_name();
 
-	my @datapoints;
-	for my $type (qw(web mobile search robot)) {
+	my $datapoints;
+	foreach my $type('web', 'mobile', 'search', 'robot') {
 		my @entries = $vwf_log->type({ domain_name => $domain_name, type => $type });
-		push @datapoints, { y => scalar(@entries), label => $type };
-
-		if(my $logger = $self->{'logger'}) {
-			$logger->debug("$type = " . scalar(@entries));
+		$datapoints .= '{y: ' . scalar(@entries) . ", label: \"$type\"},\n";
+		if($self->{'logger'}) {
+			$self->{'logger'}->debug("$type = " . scalar(@entries));
 		}
 	}
 
-	return $self->SUPER::html({ datapoints => JSON::MaybeXS::encode_json(\@datapoints) });
+	return $self->SUPER::html({ datapoints => $datapoints });
 }
 
 1;
